0042ab4c3be02c0ecd1ea31c00d045a9152d131d
Vedran Novakovic <venovako@venovako.eu>
2017-11-06 17:52:38 +0000

    A collection of the HZ levels with the MKL direct call definitions prepended.

---
+++ src/HZ/ZIMMER.F90
@@ -0,0 +1,12 @@
+#ifdef USE_MKL
+#include "mkl_direct_call.fi"
+#endif
+
+! Hari-Zimmermann, Level 0.
+#include "ZIMMER0.F90"
+
+! Hari-Zimmermann, Level 1.
+#include "ZIMMER1.F90"
+
+! Hari-Zimmermann, Level 2.
+#include "ZIMMER2.F90"
